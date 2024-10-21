d559afc39e66be92ec6b30cc6605ce1c814f8fa8
Mouzone <sunnyliu010@gmail.com>
2024-10-21 14:04:55 -0400

    Test createUser and make test directory

---
+++ test/accountQueries.test.js
@@ -0,0 +1,20 @@
+const { expect } = require('chai')
+const { createUser } = require('../queries/accountQueries')
+const { PrismaClient } = require('@prisma/client')
+
+describe('Query Account Table', () => {
+    const prisma = new PrismaClient()
+
+    it ("should successfully insert into Account table", async () => {
+        await createUser("user", "password")
+
+        const query = { where: { username: "user" } }
+        const result = await prisma.account.findUnique(query)
+        expect(result).to.include({
+            username: "user",
+            password: "password",
+        })
+
+        await prisma.account.delete(query)
+    })
+})
