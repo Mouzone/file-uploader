b6bdfa2dd885494c0c3989bd0db5bb00fb246520
Mouzone <sunnyliu010@gmail.com>
2024-10-21 17:54:01 -0400

    Migrate schema and change session store to use prisma

--- config/session.js
+++ config/session.js
@@ -1,13 +1,17 @@
 const session = require('express-session')
-const { Pool } = require('pg')
-const pgSession = require('connect-pg-simple')(session)
+const { PrismaSessionStore } = require('@quixo3/prisma-session-store')
+const { PrismaClient } = require('@prisma/client')
 
 // todo: refactor using prisma-session store
 module.exports = session({
-    store: new pgSession({
-        pool: new Pool({ connectionString: process.env.DATABASE_URL }),
-        tableName: "Session",
-    }),
+    store: new PrismaSessionStore(
+        new PrismaClient(),
+        {
+            checkPeriod: 2 * 60 * 1000,
+            dbRecordIdIsSessionId: true,
+            dbRecordIdFunction: undefined,
+        }
+    ),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
