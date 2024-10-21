68d9f304594d7f832dd60387f4ee5c3a3c8721cf
Mouzone <sunnyliu010@gmail.com>
2024-10-21 14:37:02 -0400

    Add database to be used for express session

--- config/session.js
+++ config/session.js
@@ -1,6 +1,12 @@
 const session = require('express-session')
+const { Pool } = require('pg')
+const pgSession = require('connect-pg-simple')(session)
 
 module.exports = session({
+    store: new pgSession({
+        pool: new Pool({ connectionString: process.env.DATABASE_URL }),
+        tableName: "Session",
+    }),
     secret: process.env.SESSION_SECRET,
     resave: false,
     saveUninitialized: false,
