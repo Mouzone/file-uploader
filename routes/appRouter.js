a5ba8434394dd2e0ef7fb8bf1ebf4b55d3b0f534
Mouzone <sunnyliu010@gmail.com>
2024-10-21 11:50:53 -0400

    Create sign-up view

---
+++ routes/appRouter.js
@@ -0,0 +1,8 @@
+const { Router } = require('express')
+const appRouter = Router()
+
+// appRouter.get("/", indexGet)
+// appRouter.get("/sign-up", signUpGet)
+// appRouter.get("/log-in", logInGet)
+
+module.exports = appRouter
