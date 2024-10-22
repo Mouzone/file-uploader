5a714d9fa55b661417b815d885f09f6aa08d3aa1
Mouzone <sunnyliu010@gmail.com>
2024-10-22 13:57:06 -0400

    Add css stylings to items and a directory for the css

--- app.js
+++ app.js
@@ -3,11 +3,13 @@ const app = express()
 const appRouter = require('./routes/appRouter')
 const session = require('./config/session')
 const passport = require('./config/passport')
+const path = require("node:path");
 
 app.use(express.urlencoded({extended: true}))
-app.set("views", __dirname + "/views")
+app.set("views", path.join(__dirname, "/views"))
 app.set("view engine", "ejs")
 
+app.use(express.static(path.join(__dirname, 'css')))
 app.use(session)
 app.use(passport.initialize())
 app.use(passport.session())
