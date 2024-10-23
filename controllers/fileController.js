8256646007f5a142dee3e82b11faf65cd283d1e9
Mouzone <sunnyliu010@gmail.com>
2024-10-23 14:06:26 -0400

    Delete file from file ssytem when user deletes the file from their drive

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,5 +1,6 @@
 const path = require("node:path");
 const File = require('../queries/fileQueries')
+const fs = require('fs')
 
 module.exports.fileGet = async (req, res) => {
     const file = await File.getFileById(parseInt(req.params.file_id))
@@ -7,10 +8,10 @@ module.exports.fileGet = async (req, res) => {
 }
 
 module.exports.fileDownload = async (req, res) => {
-    const { name } = await File.getFileNameById(parseInt(req.params.file_id))
+    const { name, original_name } = await File.getFileNameById(parseInt(req.params.file_id))
     const filePath = path.join(__dirname, "../public/data/uploads", name)
 
-    res.download(filePath, name, err => {
+    res.download(filePath, original_name, err => {
         if (err) {
             res.status(400).send("File Not Found")
         }
@@ -19,8 +20,15 @@ module.exports.fileDownload = async (req, res) => {
 
 module.exports.fileDelete = async (req, res) => {
     const file_id = parseInt(req.params.file_id)
-    const { folder_id } = await File.getFileById(file_id)
+    const { name, folder_id } = await File.getFileById(file_id)
     await File.deleteFile(file_id)
+
+    const filePath = path.join(__dirname, "../public/data/uploads", name)
+    fs.unlink(filePath, (error) => {
+        if (error) {
+            console.error("Error deleting the file:", error)
+        }
+    })
 
     folder_id
         ? res.redirect(`/folder/${folder_id}`)
