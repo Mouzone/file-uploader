78450deabe2678f1b7b63eaed548aadd4c38ce12
Mouzone <sunnyliu010@gmail.com>
2024-10-23 11:22:49 -0400

    Pass the name fo the folder that is opened to folder view

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -7,11 +7,11 @@ module.exports.folderGet = async (req, res) => {
         folders: await Folder.getFoldersByParent(folder_id),
         files: await File.getFilesByFolderId(folder_id)
     }
-    const { outer_folder } = await Folder.getFolderById(folder_id)
+    const { name, outer_folder } = await Folder.getFolderById(folder_id)
     const prev_folder = outer_folder
         ? await Folder.getFolderById(outer_folder)
         : null
-    res.render("folder", { items, folder_id, prev_folder })
+    res.render("folder", { items, folder_id, prev_folder, name })
 }
 
 module.exports.folderUploadPost = async (req, res) => {
