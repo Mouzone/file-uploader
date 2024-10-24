da2a3e7a4ed1edb381b8f0ca614205ff093317f5
Mouzone <sunnyliu010@gmail.com>
2024-10-24 10:57:33 -0400

    Refactor folder view and the controller to show proper back link as well as refactor the css

--- controllers/folderController.js
+++ controllers/folderController.js
@@ -15,7 +15,10 @@ module.exports.folderGet = async (req, res) => {
         files: await File.getFilesByFolderId(folder_id)
     }
     const { name, outer_folder } = await Folder.getFolderById(folder_id)
-    res.render("folder", { items, folder_id, prev_folder: outer_folder, name })
+    const prev_folder = outer_folder
+        ? await Folder.getFolderById(outer_folder)
+        : null
+    res.render("folder", { items, folder_id, prev_folder, name })
 }
 
 module.exports.folderUploadPost = async (req, res) => {
