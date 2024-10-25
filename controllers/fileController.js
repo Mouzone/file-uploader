f00b619d84910488e8f00c9ffffc60d3164a94b5
Mouzone <sunnyliu010@gmail.com>
2024-10-25 14:00:21 -0400

    Refactor fileDelete to delete based on file path and not just from index

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,6 +1,7 @@
 const path = require("node:path");
 const File = require('../queries/fileQueries')
 const fs = require('fs')
+const Folder = require("../queries/folderQueries");
 
 module.exports.fileGet = async (req, res) => {
     if (!req.session.passport?.user) {
@@ -26,8 +27,17 @@ module.exports.fileDelete = async (req, res) => {
     const { name, folder_id } = await File.getFileById(file_id)
     await File.deleteFile(file_id)
 
-    const filePath = path.join(__dirname, "../public/data/uploads", name)
-    fs.unlink(filePath, (error) => {
+    let curr_folder_id = folder_id
+    const rootPath = `./public/data/uploads`
+    let path = `/${name}`
+    while (curr_folder_id) {
+        const curr_folder = await Folder.getFolderById(curr_folder_id)
+        path = "/" + curr_folder.name + path
+        curr_folder_id = curr_folder.outer_folder
+    }
+
+
+    fs.unlink(rootPath + path, (error) => {
         if (error) {
             console.error("Error deleting the file:", error)
         }
