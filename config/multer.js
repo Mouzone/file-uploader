468f2777fb408f64364273fe434bd6dce41e2ffc
Mouzone <sunnyliu010@gmail.com>
2024-10-25 15:30:15 -0400

    Create files with their own filenames and create record in db based off of that

--- config/multer.js
+++ config/multer.js
@@ -1,5 +1,6 @@
 const multer = require('multer')
 const Folder = require('../queries/folderQueries')
+const File = require("../queries/fileQueries");
 
 module.exports.storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -7,12 +8,30 @@ module.exports.storage = multer.diskStorage({
         const rootPath = `./public/data/uploads/`
         cb(null, rootPath + req.uploadPath)
     },
-    filename: (req, file, cb) => {
-        cb(null, file.originalname)
+    filename: async (req, file, cb) => {
+        let new_name = file.originalname
+        let curr_suffix = 0
+        let result
+
+        // have user, have folder we are creating it in
+        do {
+            if (curr_suffix > 0) {
+                new_name = new_name.split("_")[0]
+                new_name += `_${curr_suffix}`
+            }
+
+            result = await File.getFileByName(
+                new_name,
+                parseInt(req.params.folder_id)
+            )
+
+            curr_suffix++
+        } while (result.length > 0)
+
+        cb(null, new_name)
     }
 })
 
-// todo: figure out the name for the file here and save it to be used later in folderController
 module.exports.computeUploadPath = async (req, res, next) => {
     let currFolderId = parseInt(req.params.folder_id);
     let uploadPath = '';
--- controllers/folderController.js
+++ controllers/folderController.js
@@ -26,29 +26,10 @@ module.exports.folderGet = async (req, res) => {
 }
 
 module.exports.folderUploadPost = async (req, res) => {
-    const { originalname, size } = req.file
-
-    let new_name = originalname
-    let curr_suffix = 0
-    let result
-
-    // have user, have folder we are creating it in
-    do {
-        if (curr_suffix > 0) {
-            new_name = new_name.split("_")[0]
-            new_name += `_${curr_suffix}`
-        }
-
-        result = await File.getFileByName(
-            new_name,
-            parseInt(req.params.folder_id)
-        )
-
-        curr_suffix++
-    } while (result.length > 0)
+    const { filename, size } = req.file
 
     await File.createFile(
-        new_name,
+        filename,
         size,
         new Date(),
         req.session.passport.user,
