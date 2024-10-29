6e7dac2e85aabaa0e960d977bf1cc32ce3a15ac4
Mouzone <sunnyliu010@gmail.com>
2024-10-29 13:53:00 -0400

    Refactor fileController to use proper queries function and add upload path to .env

--- controllers/fileController.js
+++ controllers/fileController.js
@@ -1,4 +1,3 @@
-const path = require("node:path");
 const File = require('../queries/fileQueries')
 const fs = require('fs')
 
@@ -11,10 +10,9 @@ module.exports.fileGet = async (req, res) => {
 }
 
 module.exports.fileDownload = async (req, res) => {
-    const { name, originalName } = await File.getFileNameById(parseInt(req.params.fileId))
-    const filePath = path.join(__dirname, "../public/data/uploads", name)
+    const { name, relativeRoute } = await File.getFile(parseInt(req.params.fileId))
 
-    res.download(filePath, originalName, err => {
+    res.download(process.env.UPLOAD_ROOT_PATH + relativeRoute, name, err => {
         if (err) {
             res.status(400).send("File Not Found")
         }
@@ -23,13 +21,10 @@ module.exports.fileDownload = async (req, res) => {
 
 module.exports.fileDelete = async (req, res) => {
     const fileId = parseInt(req.params.fileId)
-    const { folderId, relativeRoute } = await File.getFileById(fileId)
+    const { folderId, relativeRoute } = await File.getFile(fileId)
     await File.deleteFile(fileId)
 
-    const rootPath = `./public/data/uploads`
-
-
-    fs.unlink(rootPath + relativeRoute, (error) => {
+    fs.unlink(process.env.UPLOAD_ROOT_PATH + relativeRoute, (error) => {
         if (error) {
             console.error("Error deleting the file:", error)
         }
