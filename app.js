7acdeabe6c0c9d100270d490db5d983b04f46be1
Mouzone <sunnyliu010@gmail.com>
2024-10-30 14:48:10 -0400

    Add comments to app.js

--- app.js
+++ app.js
@@ -5,15 +5,19 @@ const session = require('./config/session')
 const passport = require('./config/passport')
 const path = require("node:path");
 
+// express.json() so we can read json data from the body when not sent through form element
 app.use(express.json())
+// makes form data readable as js objects
 app.use(express.urlencoded({extended: true}))
+// sets folder where views are to be rendered along with the type of the views
 app.set("views", path.join(__dirname, "/views"))
 app.set("view engine", "ejs")
 
+// use express.static to give folder where public assets like css and js are in to fetch with ejs files
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(session)
 app.use(passport.initialize())
-app.use(passport.session())
+app.use(passport.session({}))
 app.use("/", appRouter)
 
 app.listen(3000, () => console.log("Listening"))
