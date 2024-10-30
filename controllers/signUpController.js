543dd15102145de0945e2fc5b0eea824eded539e
Mouzone <sunnyliu010@gmail.com>
2024-10-30 14:06:35 -0400

    Add comments and remove adding req.session.passport the id object deu to using passport deserialize

--- controllers/signUpController.js
+++ controllers/signUpController.js
@@ -5,10 +5,13 @@ const bcrypt = require("bcryptjs");
 const fs = require('fs')
 const path = require('path')
 
+// get sign up page
 module.exports.signUpGet = (req, res) => {
     res.render("sign-up", { errors: [] })
 }
 
+// prevent username from have spaces and not in use
+// have no limitations for password other than confirmPassword must match it
 const validateUser = [
     body('username')
         .matches(/^\S*$/).withMessage("Username must not contain spaces")
@@ -33,22 +36,31 @@ module.exports.signUpPost = [
     validateUser,
     async (req, res) => {
         const errors = validationResult(req)
+
+        // if errors from validation render sign up page with the errors
         if (!errors.isEmpty()) {
             return res.status(400).render("sign-up", { errors: errors.array() })
         }
+
+        // username and passwords are not taken
         const { username, password } = req.body
         try {
+            // hash password and create the record in Account table
             const hashedPassword = await bcrypt.hash(password, 10)
             await Account.createUser(username, hashedPassword)
 
+            // generate session token for the user
             req.session.regenerate( async (err) => {
                 if (err) {
                     return res.status(500).send("Error regenerating session")
                 }
             })
 
+            // get id from the new created user
             const { id } = await Account.getIdByUsername(username)
+            // create folder in the Folder table
             await Folder.createFolder(id, `${id}`, `/${id}`)
+            // create folder in the file systems
             const folderPath = path.join(__dirname, `../public/data/uploads/${id}`)
             fs.mkdir(folderPath, (error) => {
                 if (error) {
@@ -56,8 +68,7 @@ module.exports.signUpPost = [
                 }
             })
 
-            req.session.passport = { user: id }
-
+            // redirect to home folder page
             res.redirect("/")
         } catch(error) {
             console.error("Error inserting user", error)
